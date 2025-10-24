babd04c05e08ea9a5c1d87fd206bec58764f3781
Jenny Nguyen <jennayx14@yahoo.com>
2025-10-23 22:29:17 -0700

    front end css

---
+++ frontend/playlist-generator/src/App.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./components/ui/button";
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-black-500 text-5xl">Mood Playlist Generator</h1>
+  
+      <Button>Get started</Button>
+  
+    </>
+
+    
+  )
+}
+
+export default App
